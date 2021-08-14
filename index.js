c5b3a697dc3a13402446accf88cbac6df3654eee
upmelih <melihseyhan@gmail.com>
2021-08-14 14:51:06 -0400

    basic

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+let errorText = document.getElementById("error")
+
+function errorbtn(){
+    errorText.innerText = "Something went wrong, please try again"
+}
+
+/*function errorbtn(){
+
+    document.getElementById("error").innerText = "Something went wrong, please try again"   
+}*/
